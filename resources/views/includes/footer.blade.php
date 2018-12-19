a39564fd1021b5e4a9de458103a61e2f42a23546
rakaiqbalsy <rakaiqbalsy@gmail.com>
2018-12-19 19:13:59 +0700

    initial Awal

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+            <div class="container">
+                    <div class="col-sm-4 col-md-offset-4 footer-social text-center">
+                        <h3>Social networks</h3>
+                        <ul class="footer-nav">
+                            <li>
+                                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+            <div class="container-fluid footer-p">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p>
+                            Copyright©2018 - MELEK TECH
+                        </p>
+                    </div>
+                </div>
+            </div>
+</footer>
+
